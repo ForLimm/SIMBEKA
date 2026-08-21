cba11304560ea63f37f2a1c1883690bf3c56eae9
Richard <pemdes.lemusa@gmail.com>
2026-08-21 12:15:15 +0800

    feat(config): configure comprehensive Indonesian date and language formatting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Carbon\Carbon::setLocale('id');
+        // Set bahasa untuk Carbon (untuk format hari/bulan seperti "Senin", "Januari")
+        \Carbon\Carbon::setLocale(config('app.locale'));
+        
+        // Set locale PHP secara global (mempengaruhi fungsi date() bawaan jika digunakan)
+        setlocale(LC_TIME, 'id_ID', 'id_ID.utf8', 'id_ID.UTF-8', 'Indonesian');
+        
+        // Pastikan timezone mengikuti konfigurasi aplikasi
+        date_default_timezone_set(config('app.timezone'));
     }
 }
